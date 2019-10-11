93a88c01e40d89ec6925d17fd272fc7ceffe6268
sundlee <sundlee@ncsoft.com>
2019-10-11 16:13:43 +0900

    20191011 1급함수 추가

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,13 @@
-@echo off
+@echo on
 
 :: build
-npm run build
+CMD /C npm run build
 
 :: navigate into the build output directory
-cd docs/.vuepress/dist
+cd docs\.vuepress\dist
 
 git init
 git add -A
-git commit -m 'deploy with vuepress'
+git commit -m "deploy with vuepress"
 
 git push -f https://github.com/sundlee/javascript-note.git master:gh-pages
-
-cd -
